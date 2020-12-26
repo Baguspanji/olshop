88f381cdb58243bd25b958bdd4e29b5afac21252
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2020-12-26 12:14:45 +0700

    as

--- index.php
+++ index.php
@@ -1,57 +1,90 @@
-<?php include('layouts/header.php') ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- ======= Hero Section ======= -->
-<section id="hero">
-  <div class="hero-container">
-    <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-      <div class="carousel-inner" role="listbox">
+	<title>ZahOlshop</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
 
-        <!-- Slide 1 -->
-        <div class="carousel-item active">
-          <div class="carousel-background"><img src="assets/img/slide/kosmetik.jpg" alt=""></div>
-          <div class="carousel-container">
-            <div class="carousel-content">
-              <h2 class="animate__animated animate__fadeInDown">Selamat datang di<br><span>ZahOlshop</span></h2>
-              <div class="row justify-content-center ">
-                <div class="social-links">
-                  <a href="#" class="twitter" style="background-color: skyblue;"><i class="bx bxl-twitter"></i></a>
-                  <a href="https://www.facebook.com/profile.php?id=100006652114555" class="facebook bg-primary"><i class="bx bxl-facebook"></i></a>
-                  <a href="https://www.instagram.com/zah_izzah/" class="instagram bg-danger"><i class="bx bxl-instagram"></i></a>
-                  <a href="https://wa.me/6281615115674?text=Saya%20tertarik%20dengan%20barang%20yang%20Anda%20jual" class="whatsapp bg-success"><i class="bx bxl-whatsapp"></i></a>
-                </div>
-              </div>
-              <!-- <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p> -->
-              <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Tentang Zah Olshop</a> -->
-            </div>
-          </div>
-        </div>
+	<!-- Favicons -->
+	<link href="assets/img/zah.jpg" rel="icon">
+	<link href="assets/img/zah.jpg" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+	<link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" />
 
 
-      </div>
+</head>
 
-    </div>
-  </div>
-</section><!-- End Hero -->
+<body>
+	<!-- ======= Hero Section ======= -->
+	<section id="hero">
+		<div class="hero-container">
+			<div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
 
-<main id="main">
+				<div class="carousel-inner" role="listbox">
 
-  <!-- ======= About Us Section ======= -->
-  <section id="about" class="about">
-    <div class="container">
+					<!-- Slide 1 -->
+					<div class="carousel-item active">
+						<div class="carousel-background"><img src="assets/img/slide/kosmetik.jpg" alt=""></div>
+						<div class="carousel-container">
+							<div class="carousel-content">
+								<h2 class="animate__animated animate__fadeInDown">Selamat datang di<br><span>ZahOlshop</span></h2>
+								<div class="row justify-content-center ">
+									<div class="social-links">
+										<a href="#" class="twitter" style="background-color: skyblue;"><i class="bx bxl-twitter"></i></a>
+										<a href="https://www.facebook.com/profile.php?id=100006652114555" class="facebook bg-primary"><i class="bx bxl-facebook"></i></a>
+										<a href="https://www.instagram.com/zah_izzah/" class="instagram bg-danger"><i class="bx bxl-instagram"></i></a>
+										<a href="https://wa.me/6281615115674?text=Saya%20tertarik%20dengan%20barang%20yang%20Anda%20jual" class="whatsapp bg-success"><i class="bx bxl-whatsapp"></i></a>
+									</div>
+								</div>
+								<!-- <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p> -->
+								<!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Tentang Zah Olshop</a> -->
+							</div>
+						</div>
+					</div>
 
-      <div class="section-title">
-        <h2>Profil ZahOlshop</h2>
-        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-      </div>
 
-      <div class="row">
-        <div class="col-lg-6">
-          <img src="assets/img/zah1.jpg" class="img-fluid" alt="">
-        </div>
-        <div class="col-lg-6 pt-4 pt-lg-0 content">
-          <h3>Voluptatem dignissimos <strong>provident quasi corporis voluptates</strong></h3>
-          <!-- <p class="font-italic">
+				</div>
+
+			</div>
+		</div>
+	</section><!-- End Hero -->
+
+	<main id="main">
+
+		<!-- ======= About Us Section ======= -->
+		<section id="about" class="about">
+			<div class="container">
+
+				<div class="section-title">
+					<h2>Profil ZahOlshop</h2>
+					<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-6">
+						<img src="assets/img/zah1.jpg" class="img-fluid" alt="">
+					</div>
+					<div class="col-lg-6 pt-4 pt-lg-0 content">
+						<h3>Voluptatem dignissimos <strong>provident quasi corporis voluptates</strong></h3>
+						<!-- <p class="font-italic">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
             </p>
@@ -60,70 +93,101 @@
               velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
             </p> -->
 
-        </div>
-      </div>
+					</div>
+				</div>
 
-    </div>
-  </section><!-- End About Us Section -->
+			</div>
+		</section><!-- End About Us Section -->
 
-  <!-- ======= Contact Us Section ======= -->
-  <section id="contact" class="contact section-bg">
+		<!-- ======= Contact Us Section ======= -->
+		<section id="contact" class="contact section-bg">
 
-    <div class="container">
-      <div class="section-title">
-        <h2>Kontak Kami</h2>
-        <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
-      </div>
-    </div>
+			<div class="container">
+				<div class="section-title">
+					<h2>Kontak Kami</h2>
+					<!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
+				</div>
+			</div>
 
-    <div class="container-fluid">
+			<div class="container-fluid">
 
-      <div class="row justify-content-md-around">
+				<div class="row justify-content-md-around">
 
-        <div class="col-md-4 info flex-column align-items-stretch">
-          <i class="bx bx-map"></i>
-          <h4>Alamat</h4>
-          <p>Tegalan, Bakalan,<br>Purwosari, Pasuruan, Jawa Timur</p>
-        </div>
-        <div class="col-md-4 info info-bg flex-column align-items-stretch">
-          <i class="bx bx-phone"></i>
-          <h4>Telp/WA</h4>
-          <p>0816-1511-5674</p>
-        </div>
+					<div class="col-md-4 info flex-column align-items-stretch">
+						<i class="bx bx-map"></i>
+						<h4>Alamat</h4>
+						<p>Tegalan, Bakalan,<br>Purwosari, Pasuruan, Jawa Timur</p>
+					</div>
+					<div class="col-md-4 info info-bg flex-column align-items-stretch">
+						<i class="bx bx-phone"></i>
+						<h4>Telp/WA</h4>
+						<p>0816-1511-5674</p>
+					</div>
 
-      </div>
+				</div>
 
-      <div class="row justify-content-center mt-lg-5">
+				<div class="row justify-content-center mt-lg-5">
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.3332527250709!2d112.74490482923214!3d-7.754460670809747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwNDUnMTYuMSJTIDExMsKwNDQnNDMuNiJF!5e0!3m2!1sen!2sid!4v1608923692503!5m2!1sen!2sid" width="1600" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.3332527250709!2d112.74490482923214!3d-7.754460670809747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zN8KwNDUnMTYuMSJTIDExMsKwNDQnNDMuNiJF!5e0!3m2!1sen!2sid!4v1608923692503!5m2!1sen!2sid" width="1600" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
-      </div>
+				</div>
 
-    </div>
-  </section><!-- End Contact Us Section -->
+			</div>
+		</section><!-- End Contact Us Section -->
 
-</main><!-- End #main -->
+	</main><!-- End #main -->
 
-<!-- ======= Footer ======= -->
-<footer id="footer">
-  <div class="footer-top">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="social-links">
-          <a href="#" class="twitter" style="background-color: skyblue;"><i class="bx bxl-twitter"></i></a>
-          <a href="https://www.facebook.com/profile.php?id=100006652114555" class="facebook bg-primary"><i class="bx bxl-facebook"></i></a>
-          <a href="https://www.instagram.com/zah_izzah/" class="instagram bg-danger"><i class="bx bxl-instagram"></i></a>
-          <a href="https://wa.me/6281615115674?text=Saya%20tertarik%20dengan%20barang%20yang%20Anda%20jual" class="whatsapp bg-success"><i class="bx bxl-whatsapp"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
+	<!-- ======= Footer ======= -->
+	<footer id="footer">
+		<div class="footer-top">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="social-links">
+						<a href="#" class="twitter" style="background-color: skyblue;"><i class="bx bxl-twitter"></i></a>
+						<a href="https://www.facebook.com/profile.php?id=100006652114555" class="facebook bg-primary"><i class="bx bxl-facebook"></i></a>
+						<a href="https://www.instagram.com/zah_izzah/" class="instagram bg-danger"><i class="bx bxl-instagram"></i></a>
+						<a href="https://wa.me/6281615115674?text=Saya%20tertarik%20dengan%20barang%20yang%20Anda%20jual" class="whatsapp bg-success"><i class="bx bxl-whatsapp"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
 
-  <div class="container">
-    <div class="copyright">
-      &copy; Copyright All Rights Reserved
-    </div>
-  </div>
-</footer><!-- End Footer -->
+		<div class="container">
+			<div class="copyright">
+				&copy; Copyright All Rights Reserved
+			</div>
+		</div>
+	</footer><!-- End Footer -->
 
-<?php include('layouts/footer.php') ?>
+	<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+	<!-- Vendor JS Files -->
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+	<script src="assets/vendor/php-email-form/validate.js"></script>
+	<script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+	<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+	<script src="assets/vendor/counterup/counterup.min.js"></script>
+	<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+	<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+	<script src="assets/vendor/venobox/venobox.min.js"></script>
+	<!-- Template Main JS File -->
+	<script src="assets/js/main.js"></script>
+	<!-- DataTables -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+
+	<!-- The core Firebase JS SDK is always required and must be listed first -->
+	<!-- <script src="https://www.gstatic.com/firebasejs/5.8.0/firebase.js"></script> -->
+	<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-auth.js"></script>
+
+	<script src="https://www.gstatic.com/firebasejs/7.23.0/firebase-analytics.js"></script>
+	<!-- Initialize Firebase -->
+	<script src="config/firebase.js"></script>
+	<script src="config/data.js"></script>
+
+</body>
+
+</html>
